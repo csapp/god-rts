6263b0f630e9e6684682d39557c181dfb1d7edf0
sappcj <sappcj@9950bdfa-aed6-41c0-9f1a-801977f84617>
2011-09-30 00:14:56 +0000

    Adding directories for source code and docs. Added an empty mod file structure to src/trunk with some edited metadata.

---
+++ modinfo.lua
@@ -0,0 +1,17 @@
+local modinfo = {
+	name = "Gods RTS",
+	shortname = "Gods",
+	game = "Gods RTS mod",
+	shortgame = "Gods",
+	description = "Team Not Enough Minerals SE 4GP6 Capstone game",
+	url = "",
+	
+	modtype = "1",
+
+	depend = {
+		"cursors.sdz",
+		"springcontent.sdz",
+	}
+}
+
+return modinfo
