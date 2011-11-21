cb0ca4dce327fac089e33f1eb54c0b350e3f4762
Manivanna Thevathasan <manixzz@hotmail.com>
2011-11-21 02:47:00 +0000

    Added walking script for warrior unit

---
+++ mods/scripts/warriorscript.lua
@@ -0,0 +1,58 @@
+local body = piece "Body"
+local head = piece "Head"
+local larm = piece "Left Arm"
+local rarm = piece "Right Arm"
+local lleg = piece "Left Leg"
+local rleg = piece "Right Leg"
+
+-- declares all the pieces we'll use in the script.
+
+local SIG_WALK = 1	--signal for the walk animation thread
+local SIG_AIM = 2  --signal for the weapon aiming thread
+local leg_movespeed = 20
+local leg_movedistance = 10
+
+
+local RESTORE_DELAY = Spring.UnitScript.GetLongestReloadTime(unitID) * 2
+-- picks a sensible time to wait before trying to turn the turret back to default.
+
+---WALKING---
+local function walk()
+	Signal(SIG_WALK)
+	SetSignalMask(SIG_WALK)
+	while (true) do
+		--left leg up, right leg down
+		Move (lleg, y_axis, leg_movedistance, leg_movespeed)
+		Move (rleg, y_axis, 0, leg_movespeed)
+		WaitForMove (lleg, y_axis)
+		WaitForMove (lleg, y_axis)	
+		Sleep (50)
+		--left leg down, right leg up
+		Move (lleg, y_axis, 0, leg_movespeed)
+		Move (rleg, y_axis, leg_movedistance, leg_movespeed)
+		WaitForMove (lleg, y_axis)
+		WaitForMove (lleg, y_axis)
+		Sleep (50)
+	end
+end
+
+
+function script.Create()
+    return 0
+end
+
+local function legs_down ()
+	Move (lleg, y_axis, 0, leg_movespeed)
+	Move (rleg, y_axis, 0, leg_movespeed)
+end
+
+function script.StartMoving()
+--    Spring.Echo ("starting to walk!")
+	StartThread (walk)
+end
+
+function script.StopMoving()
+--    Spring.Echo ("stopped walking!")
+		Signal(SIG_WALK)
+		legs_down ()
+end
