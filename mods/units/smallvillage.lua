95e2bb7384050a01b418198f9ed266060c24c2e7
Cameron Sapp <cameronsapp@gmail.com>
2012-02-03 18:30:33 +0000

    Refs #55: Adding a rough unitdef for a small village

---
+++ mods/units/smallvillage.lua
@@ -0,0 +1,52 @@
+local unitName = "smallvillage"
+
+local unitDef =
+{
+-- Internal settings	
+	Category = "LAND",
+	ObjectName = "warrior.s3o",	
+	TEDClass = "PLANT",
+	script = "smallvillage.lua",
+	buildPic = "placeholder.png",
+
+-- Unit limitations and properties
+	Description = "A small village that can build Level 1 units",
+	MaxDamage = 1500,
+	Name = "Small Village",
+	RadarDistance = 0,
+	SightDistance = 400,	
+	Upright = 1,	
+	levelground = 1,
+    capturable = true,
+	--cost
+	buildCostMetal = 0,
+	buildCostEnergy = 0,
+	buildTime = 10,
+	--economy	
+	EnergyStorage = 0,
+	EnergyUse = 0,
+	MetalStorage = 0,
+	EnergyMake = 0, 
+	MakesMetal = 0, 
+	MetalMake = 10,	
+-- Pathfinding and related
+	FootprintX = 5,
+	FootprintZ = 5,
+	MaxSlope = 10,	
+	YardMap ="ooooo occco occco occco occco",
+
+-- Building	
+	Builder = true,
+    Reclaimable = false,
+	ShowNanoSpray = true,
+	CanBeAssisted = false,	
+	workerTime = 1,
+	buildoptions = 
+	{
+        --"soldier",
+        "warrior",
+        --"general",
+	},
+}
+
+return lowerkeys({ [unitName] = unitDef })
