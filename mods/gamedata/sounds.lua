24a6bbc5325785309fbcc118f57956d026b38db9
Matthew Randell <randellma@gmail.com>
2012-02-08 06:58:32 +0000

    Fized a minor issue in sounds.lua. The curly brace still isn't closed even though it should be. It's not affecting anything for now so I'm going to leave it. Especially since I just figured out the sounds.lua file doesn't seem to be required for sounds to be imported anyway.

--- mods/gamedata/sounds.lua
+++ mods/gamedata/sounds.lua
@@ -664,38 +664,6 @@ local Sounds = {
             maxconcurrent = 1,
         },
 
---WEAPONS
---[[
-		gclaser2_fire = { 
-            file = "sounds/gclaser2_fire.wav", 
-			rolloff=3, dopplerscale = 0, maxdist = 6000,
-
-
-			priority = 10, --- higher numbers = less chance of cutoff
-			maxconcurrent = 2, ---how many maximum can we hear?
-		},
-]]--
-		--[[DefaultsForSounds = { -- this are default settings
-			file = "ThisEntryMustBePresent.wav",
-			gain = 1.0,
-			pitch = 1.0,
-			priority = 0,
-			maxconcurrent = 16, --- some reasonable limits
-			--maxdist = FLT_MAX, --- no cutoff at all
-		},
-		--- EXAMPLE ONLY!
-		MyAwesomeSound = {			
-			file = "sounds/booooom.wav",
-			preload, -- put in memory!
-			loop,  -- loop me!
-			looptime=1000, --- milliseconds!
-			gain = 2.0, --- for uber-loudness
-			pitch = 0.2, --- bass-test
-			priority = 15, --- very high
-			maxconcurrent = 1, ---only once
-			--maxdist = 500, --- only when near
-		},]]--
-
 	},
 
 }
