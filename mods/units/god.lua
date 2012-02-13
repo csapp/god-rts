9c603a82dd6dcbe450fae83093b38b751c6e605d
Manivanna Thevathasan <manixzz@hotmail.com>
2012-02-13 03:56:46 +0000

    Adding attacking, dying and moving scripts to the god unit.

--- mods/units/god.lua
+++ mods/units/god.lua
@@ -1,5 +1,12 @@
 local unitName  =  "god"
 
+--Attribute Defintions
+local HP = 2000
+local ATKDMG = 20
+local ATKSPD = 2
+local ATKRNG = 20
+local MOVESPD = {3,0.15} -- {walkspeed, acceleration}
+
 local unitDef  =  {
 --Internal settings
     BuildPic = "filename.bmp",
@@ -16,33 +23,35 @@ local unitDef  =  {
     },
     
 --Unit limitations and properties
-    BuildTime = 1000,
-    Description = "An awesomely powerful god",
-    MaxDamage = 800,
-    idleAutoHeal = 0,
+    BuildTime = 5,
+    Description = "An awesomely powerful soldier",
+    MaxDamage = HP,
+	mass = 500,
     RadarDistance = 0,
-    SightDistance = 400,
+    SightDistance = 400,	--This may be too high
     SoundCategory = "TANK",
     Upright = 0,
+    idleAutoHeal = 0,
     
 --Energy and metal related
     BuildCostEnergy = 0,
     BuildCostMetal = 0,
     
 --Pathfinding and related
-    Acceleration = 0.15,
+    Acceleration = MOVESPD[2],
     BrakeRate = 0.1,
-    FootprintX = 2,
-    FootprintZ = 2,
+    FootprintX = 1,	--Affects Bounding Box (the green colored one)
+    FootprintZ = 1,	--Affects Bounding Box (the green colored one)
     MaxSlope = 15,
-    MaxVelocity = 2.0,
+    MaxVelocity = MOVESPD[1],
     MaxWaterDepth = 20,
     MovementClass = "Default2x2",
     TurnRate = 900,
     
 --Abilities
     Builder = 0,
-    CanAttack = 1,
+    canAttack = true,
+	canFight = true,
     CanGuard = 1,
     CanMove = 1,
     CanPatrol = 1,
@@ -51,12 +60,47 @@ local unitDef  =  {
     Reclaimable = 0,
     
 --Hitbox
---    collisionVolumeOffsets    =  "0 0 0",
---    collisionVolumeScales     =  "20 20 20",
---    collisionVolumeTest       =  1,
---    collisionVolumeType       =  "box",
+	collisionVolumeOffsets = "0 0 0",
+	collisionVolumeScales = "10 25 10",
+	collisionVolumeType = "Box",
+	collisionVolumeTest = 1,
     
 --Weapons and related
+
+	weapons = {
+		[1] = {
+			def = "ARM",
+			mainDir = "0 0 1",
+			maxAngleDif = 180,
+		},
+	},
+	
+	weaponDefs = {
+	ARM = {
+		avoidFriendly = 1,
+		collideFriendly = false,
+		name = "Arm",
+		cylinderTargetting = 1,
+		energypershot = 0,
+		endsmoke = "0",
+		impactonly = true,
+		noSelfDamage = true,
+		range = ATKRNG,
+		reloadtime = ATKSPD,
+		size = 0,
+		targetBorder = 1,
+		tolerance = 5000,
+		turret = true,
+		weaponTimer = 0.1,
+		weaponType = "Cannon",
+		weaponVelocity = 100,
+		damage = {
+			default = ATKDMG,
+		},
+	},
+	
+	},
+	
     BadTargetCategory = "NOTAIR",
     ExplodeAs = "TANKDEATH",
     NoChaseCategory = "AIR",
