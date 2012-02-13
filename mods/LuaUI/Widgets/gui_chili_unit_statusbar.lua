9e8018af6b9d88dbbec6fb987e4518fa13da1bc7
Kaitlin Smith <smith.kaitlin@gmail.com>
2012-02-13 02:27:36 +0000

    More or less fixes #29. The very basic information is there. I couldn't figure out a way to update the unit portrait on the fly, so there is a static soldier placeholder there for now.

--- mods/LuaUI/Widgets/gui_chili_unit_statusbar.lua
+++ mods/LuaUI/Widgets/gui_chili_unit_statusbar.lua
@@ -17,7 +17,7 @@ function widget:GetInfo()
 end
 
 -- Uncomment this to display debug messages:
---DEBUG = 0
+DEBUG = 0
 
 -- INCLUDES
 VFS.Include("LuaRules/Includes/utilities.lua")
@@ -38,48 +38,88 @@ local spGetUnitExp			    = Spring.GetUnitExperience
 
 local game_start = false
 
+local imageName = ''
+local fileName = 'LuaUI/Widgets/images/' .. imageName .. '.png'
+
 -- SCRIPT FUNCTIONS
+-- Returns the caption, parent container and commandtype of the button	
+
 function setUnitInfo(unit)
-	local unitInfoString = printUnitHealth(unit) .. "      " .. printUnitName(unit) .. "      " .. printUnitLevel(unit) .. "      " .. printEXP(unit)
+	local unitInfoString = printUnitName(unit) .. "\n" .. printDescription(unit) .. "\n" .. printUnitLevel(unit) .. "\n" .. printUnitHealth(unit) .. "\n" .. printEXP(unit)
 
 	unitInfo:SetCaption(unitInfoString)
 end
 
-function widget:Initialize()
+--[[
+function setUnitStats(unit)
+	local unitStatString = printVelocity(unit)
 	
+	unitStats:SetCaption(unitStatString)
+end
+]]
+
+function widget:Initialize()
 	if (not WG.Chili) then
 		widgetHandler:RemoveWidget()
 		return
 	end
-
+	
 	Chili = WG.Chili
 	Label = Chili.Label
 	Window = Chili.Window
-	local screen0 = Chili.Screen0
+	Control = Chili.Control
+	Image = Chili.Image
+	
+	local screen0 = Chili.Screen0	
+
+	unitInfo = Label:New{
+		parent = statusBar,
+		x = 110,
+		width = "25%",
+		height = "100%",
+		fontsize = 13,
+		autosize = false,
+		autoObeyLineHeight = true,
+		textColor = {1,1,1,1},
+		anchors = {left=true,bottom=true,right=false,top=false},
+		caption = "",
+	}
+	--[[
+	unitStats = Label:New{
+		parent = statusBar,
+		x = 300,
+		y = -220,
+		width = "25%",
+		height = "100%",
+		fontsize = 13,
+		autosize = false,
+		autoObeyLineHeight = true,
+		textColor = {1,1,1,1},
+		anchors = {left=true,bottom=true,right=false,top=false},
+		caption = "",
+	}
+	]]
+
+	unitPic = Image:New {
+		x = 10,
+		width = 96,
+		height = 96,
+		file = 'unitpics/soldier.png',
+	}
 
 	statusBar = Window:New{
 		parent = screen0,
 		x = 0,
-		y = -220,
+		y = -150,
 		width = "100%",
-		height = "25%",
+		height = 150,
 		draggable = false,
 		resizable = false,
 		dragUseGrip = false,		
 		anchors = {left=true,bottom=true,right=true,top=false},
 		skinName  = "DarkGlass",
-	}	
-
-	unitInfo = Label:New{
-		parent = statusBar,
-		width = 1000,
-		caption = "0",
-		fontsize = 13,
-		autosize = false,
-		autoObeyLineHeight = false,
-		textColor = {1,1,1,1},
-		anchors = {left=true,bottom=true,right=false,top=false},		
-	}	
+		children = {unitInfo,unitPic,--[[unitStats]]},
+	}
 end
 
 function widget:GameStart()
@@ -87,24 +127,32 @@ function widget:GameStart()
 end
 
 function widget:CommandsChanged()
-    if not game_start then
+    local selected_units = Spring.GetSelectedUnits()
+	Spring.Echo('Commands changed')
+	if not game_start then
         return
     end
     if Spring.GetSelectedUnitsCount() == 0 then
         unitInfo:SetCaption("")
+		--unitStats:SetCaption("")
         return
     end
-    local selected_units = Spring.GetSelectedUnits()
     -- XXX need to decide what to do if multiple units are selected
 	-- This just handles showing the first unit selected in a group
     setUnitInfo(selected_units[1])
+	setUnitName(selected_units[1])
+	-- setUnitStats(selected_units[1])
+	Spring.Echo('Unit name again: ', imageName)
 end
-
+	
 function widget:Shutdown()
   widgetHandler:ConfigLayoutHandler(nil)
   Spring.ForceLayoutUpdate()
 end
 
+function setUnitName(unitID)
+	imageName = string.lower(printUnitName(unitID))
+end
 
 -- String-building functions
 
@@ -120,10 +168,10 @@ function printUnitLevel(unitID)
 end
 
 function printUnitName(unitID)
-	local defId = Spring.GetUnitDefID(unitID)
+	defId = Spring.GetUnitDefID(unitID)
 	
 	if(defId ~= nil) then
-        local unitDef = UnitDefs[defId]
+        unitDef = UnitDefs[defId]
 		unitName = unitDef.name:gsub("^%l", string.upper)
         return unitName
 	else
@@ -153,3 +201,34 @@ function printEXP(unitID)
 		return ""
 	end
 end
+
+-- This doesn't work right now. Possible workaround:
+-- http://springrts.com/phpbb/viewtopic.php?f=14&t=27554&p=512504&hilit=unit+description#p512504
+-- However I can't get that to work either
+function printDescription(unitID)
+	local desc = UnitDefs[Spring.GetUnitDefID(unitID)].description
+	
+	if desc ~= nil then
+		return desc
+	else
+		return ""
+	end
+end
+
+--[[
+function printVelocity(unitID)
+	local velocity = UnitDefs[Spring.GetUnitDefID(unitID)].MaxWaterDepth
+	
+	Spring.Echo("velocity: ", velocity)
+	
+	if velocity ~= nil then
+		return "Speed: " .. velocity
+	else
+		return ""
+	end
+end
+]]
+
+	--local range = WeaponDefs[Spring.GetUnitDefID(unitID)].range
+	--local damage = WeaponDefs[Spring.GetUnitDefID(unitID)].damge
+	--local attackSpeed = WeaponDefs[Spring.GetUnitDefID(unitID)].reloadtime
