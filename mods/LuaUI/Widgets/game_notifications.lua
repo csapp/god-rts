65228c5927b68b62c62494ccae6aa2f77650a695
Manivanna Thevathasan <manixzz@hotmail.com>
2012-03-22 01:34:40 +0000

    Adding widget which will provide notifications whenever certain events occur. Notifications can be messages, sounds  or a combination of the two. Right now this widget notifies the user when units die, level up, are damaged or are built. More cases can be added as we go on. Placeholder sounds are coded in right now. Please feel free to change them. Fixes #78, #119.

---
+++ mods/LuaUI/Widgets/game_notifications.lua
@@ -0,0 +1,83 @@
+--some of this code was adapted from author 'knorke'. It can be found here:
+--http://pastebin.com/sefpiDGq
+
+function widget:GetInfo()
+    return {
+        name = "Notification widget",
+        desc = "Widget that issues notifications when events occur.",
+        author = "Mani",
+        date = "2012-03-21",
+        license = "Public Domain",
+        layer = 0,
+        enabled = true
+    }
+end
+
+local warningDelay = 30 * 5     --in frames
+local lastWarning = 0                   --in frames
+local localTeamID = Spring.GetLocalTeamID ()
+local soundCounter
+
+local listOfUnitDamagedSounds = {
+	"sounds/fanfare10.wav",
+}
+
+local listOfUnitDestroyedSounds = {
+	"sounds/aahhh.wav",
+	"sounds/Manscreaming2.wav",
+}
+
+local listOfUnitFromFactorySounds = {
+	"sounds/gong.wav",
+}
+
+local listOfUnitLevelUpSounds = {
+	"sounds/harp.wav"
+}
+
+function unitName(unitID)
+        if (not unitID) then return nil end
+        local unitDefID = Spring.GetUnitDefID(unitID)
+        if (unitDefID) then
+                local unitDef = UnitDefs[unitDefID]
+                if (unitDef) then
+                        return unitDef.name
+                end
+        end
+        return nil
+end
+ 
+function widget:UnitDamaged (unitID, unitDefID, unitTeam, damage, paralyzer, weaponID, attackerID, attackerDefID, attackerTeam)
+        local currentFrame = Spring.GetGameFrame ()
+        if (lastWarning+warningDelay > currentFrame) then              
+                return
+        end
+        if (localTeamID == unitTeam and not Spring.IsUnitInView(unitID)) then
+                lastWarning = currentFrame
+                local attackedUnit = unitName (unitID) or "unit"
+                Spring.Echo ("Your " .. attackedUnit  .." is being attacked!")
+				soundCounter = math.random(#listOfUnitDamagedSounds)
+                Spring.PlaySoundFile (listOfUnitDamagedSounds[soundCounter])
+                local x,y,z = Spring.GetUnitPosition (unitID)
+                if (x and y and z) then Spring.SetLastMessagePosition (x,y,z) end
+        end
+end
+
+function widget:UnitDestroyed(unitID, unitDefID, teamID, attackerID, attackerDefID, attackerTeamID)
+	if Spring.IsUnitInView(unitID) and attackerID ~= nil then
+		soundCounter = math.random(#listOfUnitDestroyedSounds)
+		Spring.PlaySoundFile(listOfUnitDestroyedSounds[soundCounter])
+	elseif not Spring.IsUnitInView(unitID) and attackerID == nil then
+		soundCounter = math.random(#listOfUnitLevelUpSounds)
+		Spring.PlaySoundFile(listOfUnitLevelUpSounds[soundCounter])
+	end
+end
+
+function widget:UnitFromFactory(unitID, unitDefID, unitTeam, factID, factDefID, userOrders)
+	if not Spring.IsUnitInView(unitID) then
+		soundCounter = math.random(#listOfUnitFromFactorySounds)
+		local builtUnit = unitName (unitID) or "unit"
+		Spring.Echo ("Your " .. builtUnit  .." is ready for battle.")
+		Spring.PlaySoundFile(listOfUnitFromFactorySounds[soundCounter])
+	end
+end
