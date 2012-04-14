8b22195d644270402c179388662c7e4127b39861
Manivanna Thevathasan <manixzz@hotmail.com>
2012-04-14 02:44:23 +0000

    Minor clean up

--- mods/LuaRules/Classes/Powers/rangedpower.lua
+++ mods/LuaRules/Classes/Powers/rangedpower.lua
@@ -27,7 +27,6 @@ function RangedPower:GetRange() return self.range end
 
 function RangedPower:InRange(point)
     local x,y,z = unpack(point)
-    --local allyID = 0 -- XXX why does this work
 	local godID = self:GetGodID()
 	local allyID = Spring.GetUnitAllyTeam(godID)
     if not IsPosInLos(x, y, z, allyID) then
@@ -39,7 +38,6 @@ function RangedPower:InRange(point)
         return true
     end
 
-    local godID = self:GetGodID()
     gx, gy, gz = GetUnitBasePosition(godID)
     return utils.distance_between_points(point, {gx, gy, gz}) <= range
 end
